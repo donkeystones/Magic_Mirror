1d94427c4f75bd9891bd2f63894678e7b645dbe0
Oscar Bäck <oscar.back96@outlook.com>
2024-03-28 11:17:50 +0100

    removed react strictmode, added weather interval
    interval is set to 15 min

--- src/components/weather.tsx
+++ src/components/weather.tsx
@@ -12,10 +12,10 @@ interface WeahterData {
 const parseWeatherData = (rawData: any): Array<WeahterData> => {
     let weatherdata: Array<WeahterData> = [];
     for(let i = 0; i < 5; i++){
-        let time = rawData.timeSeries[i].validTime;
-        let symbol = rawData.timeSeries[i].parameters[18].values[0];
-        let temp = rawData.timeSeries[i].parameters[10].values[0];
-        weatherdata.push({time: time, symbol: symbol, temp: temp});
+        let time = rawData.timeSeries[i].validTime; //Get time
+        let symbol = rawData.timeSeries[i].parameters[18].values[0]; //Get symbol id
+        let temp = rawData.timeSeries[i].parameters[10].values[0]; //Get temperature in Celsius
+        weatherdata.push({time: time, symbol: symbol, temp: temp}); //Add to array
     }
     return weatherdata;
 }
@@ -25,18 +25,24 @@ const Weather = () => {
     const [weatherData, setWeatherData] = useState<Array<WeahterData>>([]);
 
     useEffect(() => {
+        
         const fetchWeatherData = async () => {
             const data = await getMeterologicalForecast();
             const parsedData = parseWeatherData(data);
             setWeatherData(parsedData);
-        }  
-
+            console.log("lol");
+        }
+        const interval = setInterval(() => {
+            fetchWeatherData().catch(console.error)
+        }, 900000);
         fetchWeatherData().catch(console.error);
+
+        return () => clearInterval(interval);
     },[])
 
     return <div className="weather">
                 <ul>
-                {weatherData.length === 0 ? "" : weatherData.map(data => <li>{data.temp}</li>)}
+                    {weatherData.length === 0 ? "" : weatherData.map(data => <li key={data.time}>{data.temp}</li>)}
                 </ul>
             </div>
 }
