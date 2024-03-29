59bd2f7415ade7bd441174f684e33adf35aa0044
Oscar Bäck <oscar.back96@outlook.com>
2024-03-29 10:59:39 +0100

    Added api calls for resrobot stops

--- src/assets/api_calls.ts
+++ src/assets/api_calls.ts
@@ -1,7 +1,20 @@
+import { RESROBOT } from "./apikeys";
+import { VASAGATAN_KN_BORLANGE_ID, VASAPARKEN_RESECENTRUM_ID } from "./station_ids";
+
 const lon = "15.432490";
 const lat = "60.485352"
 
 export const getMeterologicalForecast = async () => 
     await fetch(`https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/${lon}/lat/${lat}/data.json`)
     .then(response => response.json())
+    .then(data => { return data });
+
+export const GetDeparturesVasagatan = async () => 
+    await fetch(`https://api.resrobot.se/v2.1/departureBoard?id=${VASAGATAN_KN_BORLANGE_ID}&format=json&accessId=${RESROBOT}`)
+    .then(response => response.json)
+    .then(data => { return data });
+
+export const GetDeparturesVasaparken = async () => 
+    await fetch(`https://api.resrobot.se/v2.1/departureBoard?id=${VASAPARKEN_RESECENTRUM_ID}&format=json&accessId=${RESROBOT}`)
+    .then(response => response.json)
     .then(data => { return data });
